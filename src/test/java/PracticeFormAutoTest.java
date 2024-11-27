ef0da03c71429bb19efc5673123320da2954a403
Ivan Novikov <pola2288@yandex.ru>
2024-11-27 10:58:09 +0300

    Again rename class

--- src/test/java/PracticeFormAutoTest.java
+++ src/test/java/PracticeFormAutoTest.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class PracticeFormAutotest {
+public class PracticeFormAutoTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
